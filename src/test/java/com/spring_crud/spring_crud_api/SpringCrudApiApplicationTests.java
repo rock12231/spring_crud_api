96eda5efaa1c8784c65bd39458ea771cb693ae04
rock12231 <37548802+rock12231@users.noreply.github.com>
2021-11-05 22:30:08 +0530

    New code upload with setup of all pakages

---
+++ src/test/java/com/spring_crud/spring_crud_api/SpringCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_crud.spring_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
