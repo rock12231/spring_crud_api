fb00e71913c6c41dae650fbae8945440bed73209
rock12231 <37548802+rock12231@users.noreply.github.com>
2021-11-08 19:42:30 +0530

    db connection

---
+++ src/main/java/com/spring_crud/spring_crud_api/model/Employee.java
@@ -0,0 +1,28 @@
+package com.spring_crud.spring_crud_api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="Employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+}
